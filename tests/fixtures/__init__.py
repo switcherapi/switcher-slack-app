52432dabb85bdf213b85a6393e165cc859eb7e5e
petruki <31597636+petruki@users.noreply.github.com>
2021-05-04 21:21:34 -0700

    Added test fixtures

---
+++ tests/fixtures/__init__.py
@@ -0,0 +1,28 @@
+INSTALLATION_FIX1 = {
+    "app_id": "app_id",
+    "enterprise_id": None,
+    "enterprise_name": None,
+    "enterprise_url": None,
+    "team_id": "team_id",
+    "team_name": "switcher",
+    "bot_token": "bot_token",
+    "bot_id": "bot_id",
+    "bot_user_id": "bot_user_id",
+    "bot_scopes": [ 
+        "chat:write", 
+        "commands", 
+        "incoming-webhook"
+    ],
+    "user_id": "user_id",
+    "user_token": "user_token",
+    "user_scopes": [ 
+        "im:history"
+    ],
+    "incoming_webhook_url": "incoming_webhook_url",
+    "incoming_webhook_channel": "incoming_webhook_channel",
+    "incoming_webhook_channel_id": "incoming_webhook_channel_id",
+    "incoming_webhook_configuration_url": "incoming_webhook_configuration_url",
+    "is_enterprise_install": bool(False),
+    "token_type": "bot",
+    "installed_at": float(1620070031.44704)
+}
--- tests/test_switcher_store.py
+++ tests/test_switcher_store.py
@@ -1,11 +1,16 @@
-import sys
-sys.path.insert(0, '../src')
-
 import pytest
+
+from pytest_mock import mocker
+from mock import Mock, patch
+
+from slack_sdk.oauth.installation_store.models.installation import Installation
 from src.store.switcher_store import SwitcherAppInstallationStore
+
+from .fixtures import INSTALLATION_FIX1
 
 class TestClass():
     store = SwitcherAppInstallationStore()
-
-    def test(self):
-        assert True
+    
+    def test_save_installation(self, mocker):
+        installation = Installation(**INSTALLATION_FIX1)
+        self.store.save(installation)
