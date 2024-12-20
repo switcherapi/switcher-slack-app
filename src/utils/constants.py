17c9948f9477bb0c4ecbca9a45c91e3ff7a02639
petruki <31597636+petruki@users.noreply.github.com>
2024-12-19 18:51:41 -0800

    Bump slack_bolt@1.22.0

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -7,4 +7,4 @@ load_dotenv()
 SWITCHER_URL = os.environ.get("SWITCHER_URL")
 SWITCHER_API_URL = os.environ.get("SWITCHER_API_URL")
 RELEASE_TIME = os.environ.get("RELEASE_TIME", "latest")
-VERSION = f"2.0.1 {RELEASE_TIME}"
+VERSION = f"2.1.0 {RELEASE_TIME}"
